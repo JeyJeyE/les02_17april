625341228fc6b23591415e3f40a3ac529da718a5
JeyJeyE <167130069+JeyJeyE@users.noreply.github.com>
2024-04-18 00:46:47 +0300

    Add files via upload

---
+++ src/com/edu/les02/main/prosto.java
@@ -0,0 +1,16 @@
+package com.edu.les02.main;
+
+public class prosto {
+
+	public static void main(String[] args) {
+
+		int x = 5;
+		int y = 3;
+		int z;
+
+		z = 2 * x + (y - 2) * 5;
+		System.out.println("z =  " + z);
+
+	}
+
+}
